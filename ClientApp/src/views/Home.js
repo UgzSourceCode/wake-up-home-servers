75e54fa08191c3296f2b783e396628c83c1f4800
Piotr 'Ugz' Bielski <pb93infa@gmail.com>
2021-02-25 23:03:38 +0100

    change class to function object

---
+++ ClientApp/src/views/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Hello, user!</h1>
+      <p>Welcome in control panel of world</p>
+      <ul>
+        <li>In this app you can add your home server address,</li>
+        <li>Wake up your all servers or some server from lists,</li>
+        <li>Delete server address.</li>
+      </ul>
+    </div>
+  );
+};
